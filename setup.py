b69c0ad553e5d23c977e30c24a339a5dbd263961
aheadley <aheadley@waysaboutstuff.com>
2010-09-10 10:51:12 -0400

    more dist stuff

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from distutils.core import setup
 
 setup(
     name='pyOpsview',
-    version='0.1',
+    version='0.1.0a1',
     description='Opsview interface and utilities.',
     author='Alex Headley',
     author_email='aheadley@waysaboutstuff.com',
     url='http://github.com/aheadley/pyopsviewremote',
+    download_url='http://github.com/aheadley/pyopsviewremote/tarball/master',
     py_modules=['opsview'],
 )
